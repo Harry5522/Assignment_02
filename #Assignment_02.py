f14cf44ec75ac6a4079f7360c762f6471aa135e9
Abdullah Alali <136125725+Harry5522@users.noreply.github.com>
2025-01-22 11:41:24 +0400

    Update #Assignment_02.py

--- #Assignment_02.py
+++ #Assignment_02.py
@@ -1,8 +1,15 @@
 #Assignment 02
 
-
+#Define the menu function
 def display_menu(menu):
     print("\nMenu:")  
     for item, price in menu.items():
-        print(f"{item}: {price} AED")  
+        print(f"{item}: {price} AED")      # Show items with price
+
+
+def calculate_total(order, menu):
+    total = 0                
+    for item in order:       
+        total += menu[item]  
+    return total             
 
