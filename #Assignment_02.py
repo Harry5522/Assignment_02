160af9902536faf9510cbe393de7d149b39d69e8
Abdullah Alali <136125725+Harry5522@users.noreply.github.com>
2025-01-23 16:50:56 +0400

    Update #Assignment_02.py

--- #Assignment_02.py
+++ #Assignment_02.py
@@ -1,7 +1,7 @@
 #Assignment 02
 
-
+# Show menu with prices
 def display_menu(menu, stock):
     print("\nMenu:")
     for item, price in menu.items():
-        print(f"{item}: {price} AED")
+        print(f"{item}: {price} AED")     # Print product name and price
